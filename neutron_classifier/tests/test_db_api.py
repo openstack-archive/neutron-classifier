3b545744d3dfd204d0806281e68958726287a8c2
Ramanjaneya <ramanjaneya.palleti@huawei.com>
2015-12-05 16:02:00 +0530

    Implement Firewall conversions
    This patch implements API's for
    * Saving firewall rules in common classifier model.
    * Retrieving firewall rule from the common classfier db.
    
    Change-Id: I64ff4ce875fd909d57375d1c601dc61e86c79f40
    Co-Authored-By: Mohankumar <mohankumar.n@huawei.com>

--- neutron_classifier/tests/test_db_api.py
+++ neutron_classifier/tests/test_db_api.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2015 Mirantis, Inc.
+# Copyright (c) 2015 Huawei Technologies India Pvt Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
@@ -32,6 +33,32 @@ FAKE_SG_RULE_V4 = {'direction': 'INGRESS', 'protocol': 'tcp', 'ethertype':
 FAKE_SG_V6 = {'name': 'fake security group', 'tenant_id':
               uuidutils.generate_uuid(), 'description': 'this is fake',
               'security_group_rules': [FAKE_SG_RULE_V6]}
+
+FAKE_FW_RULE_V4 = {'ip_version': 4, 'protocol': 'udp',
+                   'source_port_range_min': 1, 'source_port_range_max': 80,
+                   'destination_port_range_min': 1,
+                   'destination_port_range_max': 80,
+                   'source_ip_address': '20.1.1.1/24',
+                   'destination_ip_address': '30.1.1.1/24',
+                   'tenant_id': 'fake_tenant', }
+
+FAKE_FW_RULE_V6 = {'ip_version': 6, 'protocol': 'udp',
+                   'source_port_range_min': 1, 'source_port_range_max': 80,
+                   'destination_port_range_min': 1,
+                   'destination_port_range_max': 80,
+                   'source_ip_address': 'fddf:cb3b:bc4::/48',
+                   'destination_ip_address': 'fddf:cb3b:b33f::/48',
+                   'tenant_id': 'fake_tenant', }
+
+FAKE_FW_V4 = {'name': 'fake firewall policy',
+              'tenant_id': uuidutils.generate_uuid(),
+              'description': 'this is fake',
+              'firewall_rules': [FAKE_FW_RULE_V4]}
+
+FAKE_FW_V6 = {'name': 'fake firewall policy',
+              'tenant_id': uuidutils.generate_uuid(),
+              'description': 'this is fake',
+              'firewall_rules': [FAKE_FW_RULE_V6]}
 
 
 class ClassifierTestContext(object):
@@ -87,22 +114,6 @@ class DbApiTestCase(base.BaseTestCase):
     def test_convert_security_group_rule_v6_to_classifier(self):
         self._test_convert_security_group_rule_to_classifier(FAKE_SG_RULE_V6)
 
-    def test_convert_firewall_rule_to_classifier(self):
-        firewall_rule = {'protocol': 'foo',
-                         'ip_version': 6,
-                         'source_ip_address': 'fddf:cb3b:bc4::/48',
-                         'destination_ip_address': 'fddf:cb3b:b33f::/48',
-                         'source_port': 80,
-                         'destination_port': 80,
-                         'position': 1,
-                         'action': 'ALLOW',
-                         'enabled': True
-                         }
-        api.convert_firewall_rule_to_classifier(self.context, firewall_rule)
-
-    def test_convert_firewall_policy_to_classifier_chain(self):
-        pass
-
     def test_convert_security_group_to_classifier_chain(self):
         result = api.convert_security_group_to_classifier(self.context,
                                                           FAKE_SG_V6)
@@ -116,5 +127,39 @@ class DbApiTestCase(base.BaseTestCase):
         result['tenant_id'] = FAKE_SG_RULE_V6['tenant_id']
         self.assertEqual(FAKE_SG_RULE_V6, result)
 
-    def test_convert_classifier_chain_to_firewall_policy(self):
-        pass
+    # Firewall testcases
+    def _test_convert_firewall_rule_to_classifier(self, fw_rule):
+        cg = self._create_classifier_group('neutron-fwaas')
+        api.convert_firewall_rule_to_classifier(self.context, fw_rule, cg)
+
+        # Save to the database
+        self.context.session.add(cg)
+        self.context.session.commit()
+
+        # Refresh the classifier group from the DB
+        cg = api.get_classifier_group(self.context, cg.id)
+        self.assertGreater(len(cg.classifier_chain), 0)
+
+    def test_convert_firewall_rule_v4_to_classifier(self):
+        self._test_convert_firewall_rule_to_classifier(FAKE_FW_RULE_V4)
+
+    def test_convert_firewall_rule_v6_to_classifier(self):
+        self._test_convert_firewall_rule_to_classifier(FAKE_FW_RULE_V6)
+
+    def test_convert_firewall_policy_v4_to_classifier_chain(self):
+        result = api.convert_firewall_policy_to_classifier(self.context,
+                                                           FAKE_FW_V4)
+        self.assertIsNotNone(result)
+
+    def test_convert_firewall_policy_v6_to_classifier_chain(self):
+        result = api.convert_firewall_policy_to_classifier(self.context,
+                                                           FAKE_FW_V6)
+        self.assertIsNotNone(result)
+
+    def test_convert_classifier_chain_to_firewall(self):
+        classifier_id = api.convert_firewall_policy_to_classifier(
+            self.context, FAKE_FW_V6).id
+        result = api.convert_classifier_to_firewall(self.context,
+                                                    classifier_id)
+        result['tenant_id'] = FAKE_FW_RULE_V6['tenant_id']
+        self.assertEqual(FAKE_FW_RULE_V6, result)
