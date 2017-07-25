7b7e42b47e2a6a749f4262addf9f9d62d5b93c7c
Thaynara Silva <thaynara.nascimento.da.silva@intel.com>
2017-07-25 16:28:04 +0000

    [WIP] Version 0 Patch
     - All classification types expressed in a single
       resource.
     - Classifications can be grouped inside classification
       groups resource which can be consumed by other
       neutron resources.
     - A list of supported classifications exists through
       classification type list resource.
     - Switch to stestr
    
    Change-Id: I05acaf6419c294692ff270ad4a488a5e68749d67
    Co-Authored-By: David Shaughnessy <david.shaughnessy@intel.com>
    Co-Authored-By: Nakul Dahiwade <nakul.dahiwade@intel.com>

---
+++ neutron_classifier/services/classification/plugin.py
@@ -0,0 +1,176 @@
+# Copyright 2017 Intel Corporation.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_log import log as logging
+
+from neutron.db import api as db_api
+from neutron.objects import base as base_obj
+from neutron_classifier.common import exceptions
+from neutron_classifier.common import validators
+from neutron_classifier.db import classification as c_db
+from neutron_classifier.extensions import classification
+from neutron_classifier.objects import classification_type as type_obj
+from neutron_classifier.objects import classifications as class_group
+
+LOG = logging.getLogger(__name__)
+
+
+class ClassificationPlugin(classification.NeutronClassificationPluginBase,
+                           c_db.TrafficClassificationGroupPlugin):
+    supported_extension_aliases = ['neutron_classifier']
+
+    def __init__(self):
+        super(ClassificationPlugin, self).__init__()
+        self.driver_manager = None
+
+    def create_classification(self, context, classification):
+        details = self.break_out_headers(classification)
+        c_type = details['c_type']
+        headers = classification['classification']['definition']
+
+        for key in headers:
+            if key not in validators.type_validators[c_type].keys():
+                raise exceptions.InvalidClassificationDefintion()
+
+        cl = class_group.CLASS_MAP[c_type](context, **details)
+        with db_api.context_manager.writer.using(context):
+            cl.create()
+        db_dict = self.merge_header(cl)
+        db_dict['id'] = cl['id']
+
+        return db_dict
+
+    def delete_classification(self, context, classification_id):
+        cl = class_group.ClassificationBase.get_object(context,
+                                                       id=classification_id)
+        cl_class = class_group.CLASS_MAP[cl.c_type]
+        classification = cl_class.get_object(context, id=classification_id)
+        validators.check_valid_classifications(context,
+                                               [classification_id])
+
+        with db_api.context_manager.writer.using(context):
+            classification.delete()
+
+    def update_classification(self, context, classification_id,
+                              fields_to_update):
+        field_keys = fields_to_update.keys()
+        valid_keys = [['name'], ['description'], ['name', 'description']]
+        if field_keys not in valid_keys:
+            raise exceptions.InvalidClassificationGroupUpdateRequest()
+            raise exceptions.InvalidUpdateRequest()
+        cl = class_group.ClassificationBase.get_object(context,
+                                                       id=classification_id)
+        cl_class = class_group.CLASS_MAP[cl.c_type]
+        with db_api.context_manager.writer.using(context):
+            classification = cl_class.update_object(
+                context, fields_to_update, id=classification_id)
+        return classification
+
+    def get_classification(self, context, classification_id, fields=None):
+        cl = class_group.ClassificationBase.get_object(context,
+                                                       id=classification_id)
+        cl_class = class_group.CLASS_MAP[cl.c_type]
+        classification = cl_class.get_object(context, id=classification_id)
+
+        clas = self.merge_header(classification)
+        return clas
+
+    def get_classifications(self, context, filters=None, fields=None,
+                            sorts=None, limit=None, marker=None,
+                            page_reverse=False):
+        # NOTE(ndahiwad): If the filters are not passed by the end-user
+        # then will fetch all the classifications. Otherwise, only the
+        # classification_types that the user wants will be returned.
+        if not filters['c_type']:
+            filters['c_type'] = ['tcp', 'udp', 'ipv4', 'ipv6', 'ethernet']
+        c_dict = {'classifications': []}
+        for c_type in filters['c_type']:
+            pager = base_obj.Pager(sorts, limit, page_reverse, marker)
+            cl = class_group.CLASS_MAP[c_type].get_objects(context,
+                                                           _pager=pager)
+            db_dict = self.merge_headers(cl)
+            c_dict['classifications'].append(db_dict)
+
+        return c_dict
+
+    def get_classification_type(self, context, filters=None, fields=None,
+                                sorts=None, limit=None, marker=None,
+                                page_reverse=False):
+        ret_list = []
+        if not filters:
+            filters = {}
+        for key in class_group.CLASS_MAP.keys():
+            types = {}
+            obj = type_obj.ClassificationType.get_object(key)
+            types['type'] = obj.type
+            types['supported_parameters'] = obj.supported_parameters
+            ret_list.append(types)
+
+        return ret_list
+
+    def __getattr__(self, resource):
+        return super(ClassificationPlugin, self).__getattr__(resource)
+
+    def break_out_headers(self, classification):
+        details = classification['classification']
+
+        cl_dict = {'name': details['name'],
+                   'description': details['description'],
+                   'project_id': details['project_id'],
+                   'shared': details['shared'],
+                   'c_type': details['c_type'],
+                   'negated': details['negated']}
+
+        definition = details['definition']
+
+        for key, value in definition.items():
+            cl_dict[key] = value
+
+        return cl_dict
+
+    def merge_headers(self, classifications):
+        c_type = classifications[0]['c_type']
+        ret_list = {CLASSIFICATION_MAP[c_type]: []}
+
+        for clas in classifications:
+            db_dict = self.merge_header(clas)
+            db_dict['id'] = clas.get('id', None)
+            ret_list[CLASSIFICATION_MAP[c_type]].append(db_dict)
+        return ret_list
+
+    def merge_header(self, classification):
+        db_dict = {'id': classification['id'],
+                   'name': classification['name'],
+                   'project_id': classification['project_id'],
+                   'description': classification['description'],
+                   'c_type': classification['c_type'],
+                   'negated': classification['negated'],
+                   'shared': classification['shared']}
+
+        c_type = classification['c_type']
+        headers = validators.type_validators[c_type].keys()
+        definition = {}
+
+        for header in headers:
+            definition[header] = classification.get(header, None)
+
+        db_dict['definition'] = definition
+        return db_dict
+
+
+CLASSIFICATION_MAP = {'ethernet': 'EthernetClassifications',
+                      'ipv4': 'IPV4Classifications',
+                      'ipv6': 'IPV6Classifications',
+                      'udp': 'UDPClassifications',
+                      'tcp': 'TCPClassifications'}
---
+++ neutron_classifier/tests/functional/test_api.py
@@ -0,0 +1,181 @@
+# Copyright (c) 2018 Intel Corporation.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_utils import uuidutils
+
+from neutron.db import api as db_api
+from neutron.tests.unit import testlib_api
+
+from neutron_classifier.common import exceptions
+from neutron_classifier.common import validators
+from neutron_classifier.db.classification import\
+    TrafficClassificationGroupPlugin as cg_plugin
+from neutron_classifier.objects import classifications
+from neutron_classifier.services.classification.plugin import\
+    ClassificationPlugin as c_plugin
+from neutron_classifier.tests import objects_base as obj_base
+
+
+class ClassificationGroupApiTest(testlib_api.MySQLTestCaseMixin,
+                                 testlib_api.SqlTestCase,
+                                 obj_base._CCFObjectsTestCommon):
+    def setUp(self):
+        super(ClassificationGroupApiTest, self).setUp()
+        self.test_plugin = cg_plugin()
+
+    def test_get_classification_group(self):
+        cg = self._create_test_cg('Test Group 0')
+        cg_dict = self.test_plugin._make_db_dict(cg)
+        fetch_cg = self.test_plugin.get_classification_group(self.ctx, cg.id)
+        cg_dict['classification_group']['classifications'] =\
+            fetch_cg['classification_group']['classifications']
+        cg_dict['classification_group']['classification_groups'] = \
+            fetch_cg['classification_group']['classification_groups']
+        self.assertEqual(cg_dict, fetch_cg)
+
+    def test_get_classification_groups(self):
+        cg1 = self._create_test_cg('Test Group 1')
+        cg2 = self._create_test_cg('Test Group 2')
+        cgs = self.test_plugin.get_classification_groups(self.ctx)
+        self.assertIn(cg1, cgs)
+        self.assertIn(cg2, cgs)
+
+    def test_create_classification_group(self):
+        tcp_class = classifications.TCPClassification
+        ipv4_class = classifications.IPV4Classification
+        cg2 = self._create_test_cg('Test Group 1')
+        tcp = self._create_test_classification('tcp', tcp_class)
+        ipv4 = self._create_test_classification('ipv4', ipv4_class)
+        cg_dict = {'classification_group':
+                   {'name': 'Test Group 0',
+                    'description': "Description of test group",
+                    'project_id': uuidutils.generate_uuid(),
+                    'operator': 'AND',
+                    'classifications': [tcp.id, ipv4.id],
+                    'classification_groups': [cg2.id]
+                    }}
+        with db_api.context_manager.writer.using(self.ctx):
+            cg1 = self.test_plugin.create_classification_group(self.ctx,
+                                                               cg_dict)
+            fetch_cg1 = classifications.ClassificationGroup.get_object(
+                self.ctx, id=cg1['id'])
+            mapped_cgs = classifications._get_mapped_classification_groups(
+                self.ctx, fetch_cg1)
+            mapped_cs = classifications._get_mapped_classifications(
+                self.ctx, fetch_cg1)
+            mapped_classification_groups = [cg.id for cg in mapped_cgs]
+            mapped_classifications = [c.id for c in mapped_cs]
+            self.assertEqual(cg1, cg_dict['classification_group'])
+            for cg in mapped_classification_groups:
+                self.assertIn(
+                    cg,
+                    cg_dict['classification_group']['classification_groups'])
+            for c in mapped_classifications:
+                self.assertIn(
+                    c, cg_dict['classification_group']['classifications'])
+
+    def test_update_classification_group(self):
+        cg1 = self._create_test_cg('Test Group 0')
+        cg2 = self._create_test_cg('Test Group 1')
+        with db_api.context_manager.writer.using(self.ctx):
+            self.test_plugin.update_classification_group(
+                self.ctx, cg1.id, {'name': 'Test Group updated'})
+            fetch_cg1 = classifications.ClassificationGroup.get_object(
+                self.ctx, id=cg1['id'])
+            self.assertRaises(
+                exceptions.InvalidUpdateRequest,
+                self.test_plugin.update_classification_group,
+                self.ctx, cg2.id, {'name': 'Test Group updated',
+                                   'operator': 'OR'})
+            self.assertEqual(fetch_cg1.name, 'Test Group updated')
+
+    def test_delete_classification_group(self):
+        cg1 = self._create_test_cg('Test Group 0')
+        self.test_plugin.delete_classification_group(self.ctx, cg1.id)
+        fetch_cg1 = classifications.ClassificationGroup.get_object(
+            self.ctx, id=cg1['id'])
+        self.assertIsNone(fetch_cg1)
+
+
+class ClassificationApiTest(testlib_api.MySQLTestCaseMixin,
+                            testlib_api.SqlTestCase,
+                            obj_base._CCFObjectsTestCommon):
+    def setUp(self):
+        super(ClassificationApiTest, self).setUp()
+        self.test_clas_plugin = c_plugin()
+
+    def test_create_classification(self):
+        attrs = self.get_random_attrs(classifications.EthernetClassification)
+        c_type = 'ethernet'
+        attrs['c_type'] = c_type
+        attrs['definition'] = {}
+        for key in validators.type_validators[c_type].keys():
+            attrs['definition'][key] = attrs.pop(key, None)
+        c_attrs = {'classification': attrs}
+        with db_api.context_manager.writer.using(self.ctx):
+            c1 = self.test_clas_plugin.create_classification(self.ctx,
+                                                             c_attrs)
+            fetch_c1 = classifications.EthernetClassification.get_object(
+                self.ctx, id=c1['id']
+            )
+            c_attrs['classification']['definition']['src_port'] = 'xyz'
+        self.assertRaises(exceptions.InvalidClassificationDefintion,
+                          self.test_clas_plugin.create_classification,
+                          self.ctx, c_attrs)
+        eth = c1.pop('definition', None)
+        for k, v in c1.items():
+            self.assertEqual(v, fetch_c1[k])
+        for x, y in eth.items():
+            self.assertEqual(y, fetch_c1[x])
+
+    def test_delete_classification(self):
+        tcp_class = classifications.TCPClassification
+        tcp = self._create_test_classification('tcp', tcp_class)
+        self.test_clas_plugin.delete_classification(self.ctx, tcp.id)
+        fetch_tcp = classifications.TCPClassification.get_object(self.ctx,
+                                                                 id=tcp.id)
+        self.assertIsNone(fetch_tcp)
+
+    def test_get_classification(self):
+        ipv4_class = classifications.IPV4Classification
+        ipv4 = self._create_test_classification('ipv4', ipv4_class)
+        fetch_ipv4 = self.test_clas_plugin.get_classification(self.ctx,
+                                                              ipv4.id)
+        self.assertEqual(fetch_ipv4, self.test_clas_plugin.merge_header(ipv4))
+
+    def test_get_classifications(self):
+        c1 = self._create_test_classification(
+            'ipv6', classifications.IPV6Classification)
+        c2 = self._create_test_classification(
+            'udp', classifications.UDPClassification)
+        with db_api.context_manager.writer.using(self.ctx):
+            fetch_cs = self.test_clas_plugin.get_classifications(
+                self.ctx, filters={'c_type': ['udp', 'ipv6']})
+        c1_dict = self.test_clas_plugin.merge_header(c1)
+        c2_dict = self.test_clas_plugin.merge_header(c2)
+        self.assertIn({'UDPClassifications': [c2_dict]},
+                      fetch_cs['classifications'])
+        self.assertIn({'IPV6Classifications': [c1_dict]},
+                      fetch_cs['classifications'])
+
+    def test_update_classification(self):
+        c1 = self._create_test_classification(
+            'ethernet', classifications.EthernetClassification)
+        updated_name = 'Test Updated Classification'
+        with db_api.context_manager.writer.using(self.ctx):
+            self.test_clas_plugin.update_classification(self.ctx, c1.id,
+                                                        {'name': updated_name})
+            fetch_c1 = classifications.EthernetClassification.get_object(
+                self.ctx, id=c1.id)
+        self.assertEqual(fetch_c1.name, updated_name)
