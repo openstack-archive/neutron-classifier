b5be4de350625163fbdb96867de169ebcc5688bd
Thaynara Silva <thaynara.nascimento.da.silva@intel.com>
2017-08-28 12:16:14 +0000

    Add Database Migrations Classifications
     - Introducing database migrations for the neutron common
       classification framework.
    
    Change-Id: Iceca7c10aaec7a8566d994ed1acd057818e54d5d
    Co-Authored-By: David Shaughnessy <david.shaughnessy@intel.com>

---
+++ neutron_classifier/db/migration/alembic_migrations/versions/pike/contract/b4d298d743ea_initial_ccf_database_.py
@@ -0,0 +1,30 @@
+# Copyright 2017 Intel Corporation.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+"""initial CCF database contraction
+
+Revision ID: b4d298d743ea
+Revises: None
+Create Date: 2017-08-28 14:14:31.181166
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'b4d298d743ea'
+down_revision = 'start_neutron_classifier'
+
+
+def upgrade():
+    pass
