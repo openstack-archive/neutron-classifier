9850ea242ba84d0c344a1470b13a92911e370880
Thaynara Silva <silvathaynara21@gmail.com>
2017-08-31 12:07:55 +0000

    Add Database Models and OVOs for classifications
     - Introducing database models and oslo versioned objects for
       classification resources used by the common classification
       framework service plugin.
    
    Change-Id: I41d5b399352b47d74000596e6518e199d36709a7
    Co-Authored-By: David Shaughnessy <david.shaughnessy@intel.com>

--- neutron_classifier/db/models.py
+++ neutron_classifier/db/models.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2015 Mirantis, Inc.
+# Copyright 2017 Intel Corporation.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
@@ -12,161 +13,186 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from neutron_classifier.common import constants
-from oslo_utils import uuidutils
+from neutron_lib.db import model_base
+
 import sqlalchemy as sa
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.ext.orderinglist import ordering_list
-from sqlalchemy import orm
 
 Base = declarative_base()
 
 
-# Stolen from neutron/db/model_base.py
-class HasTenant(object):
-    """Tenant mixin, add to subclasses that have a tenant."""
-
-    tenant_id = sa.Column(sa.String(255), index=True)
-
-
-# Stolen from neutron/db/model_base.py
-class HasId(object):
-    """id mixin, add to subclasses that have an id."""
-    id = sa.Column(sa.String(36),
-                   primary_key=True,
-                   default=uuidutils.generate_uuid)
-
-
-class Classifier(Base, HasId):
-    __tablename__ = 'classifiers'
-    classifier_type = sa.Column(sa.String)
-    __mapper_args__ = {'polymorphic_on': classifier_type}
-
-
-class ClassifierGroup(Base, HasTenant, HasId):
-    __tablename__ = 'classifier_groups'
+# Service plugin models
+class ClassificationGroup(model_base.BASEV2, model_base.HasId,
+                          model_base.HasProject):
+    __tablename__ = 'classification_groups'
     name = sa.Column(sa.String(255))
     description = sa.Column(sa.String(255))
-    classifier_chain = orm.relationship(
-        'ClassifierChainEntry',
-        backref=orm.backref('classifier_chains', cascade='all, delete'),
-        order_by='ClassifierChainEntry.sequence',
-        collection_class=ordering_list('sequence', count_from=1))
-    service = sa.Column(sa.Enum(*constants.NEUTRON_SERVICES), index=True)
+    shared = sa.Column(sa.Boolean(), default=False)
+    operator = sa.Column(sa.Enum('AND', 'OR'), default='AND')
 
 
-class ClassifierChainEntry(Base, HasId):
-    __tablename__ = 'classifier_chains'
-    classifier_group_id = sa.Column(sa.String(36),
-                                    sa.ForeignKey('classifier_groups.id',
-                                                  ondelete="CASCADE"))
-    classifier_id = sa.Column(sa.String(36),
-                              sa.ForeignKey('classifiers.id',
-                                            ondelete="CASCADE"))
-    classifier = orm.relationship(Classifier)
-    sequence = sa.Column(sa.Integer)
-    classifier_group = orm.relationship(ClassifierGroup)
-
-    def __init__(self, classifier_group=None, classifier=None, sequence=None):
-        super(ClassifierChainEntry, self).__init__()
-        self.classifier = classifier
-        self.classifier_group = classifier_group
-        self.sequence = sequence
+class CGToClassificationMapping(model_base.BASEV2):
+    __tablename__ = 'classification_group_to_classification_mappings'
+    container_cg_id = sa.Column(sa.String(36),
+                                sa.ForeignKey('classification_groups.id'),
+                                primary_key=True)
+    stored_classification_id = sa.Column(sa.String(36),
+                                         sa.ForeignKey('classifications.id'),
+                                         primary_key=True)
 
 
-class DirectionClassifier(Classifier):
-    __tablename__ = 'direction_classifiers'
-    __mapper_args__ = {'polymorphic_identity': 'directionclassifier'}
-    id = sa.Column(sa.String(36), sa.ForeignKey('classifiers.id'),
+class CGToClassificationGroupMapping(model_base.BASEV2):
+    __tablename__ = 'classification_group_to_cg_mappings'
+    container_cg_id = sa.Column(sa.String(36),
+                                sa.ForeignKey('classification_groups.id'),
+                                primary_key=True)
+    stored_cg_id = sa.Column(sa.String(36),
+                             sa.ForeignKey('classification_groups.id'),
+                             primary_key=True)
+
+
+class ClassificationBase(Base, model_base.HasId, model_base.HasProject,
+                         model_base.BASEV2):
+    __tablename__ = 'classifications'
+    c_type = sa.Column(sa.String(36))
+    __mapper_args__ = {'polymorphic_on': c_type}
+    name = sa.Column(sa.String(255))
+    description = sa.Column(sa.String(255))
+    shared = sa.Column(sa.Boolean())
+    negated = sa.Column(sa.Boolean())
+
+
+class IPV4Classification(ClassificationBase):
+    __tablename__ = 'ipv4_classifications'
+    __mapper_args__ = {'polymorphic_identity': 'ipv4'}
+    id = sa.Column(sa.String(36), sa.ForeignKey('classifications.id'),
                    primary_key=True)
-    direction = sa.Column(sa.Enum(*constants.DIRECTIONS))
+    dscp = sa.Column(sa.Integer())
+    dscp_mask = sa.Column(sa.Integer())
+    ecn = sa.Column(sa.Enum("0", "1", "2", "3"))
+    length_min = sa.Column(sa.Integer())
+    length_max = sa.Column(sa.Integer())
+    flags = sa.Column(sa.Integer())
+    flags_mask = sa.Column(sa.Integer())
+    ttl_min = sa.Column(sa.SmallInteger())
+    ttl_max = sa.Column(sa.SmallInteger())
+    protocol = sa.Column(sa.Integer())
+    src_addr = sa.Column(sa.String(19))
+    dst_addr = sa.Column(sa.String(19))
 
-    def __init__(self, direction=None):
-        super(DirectionClassifier, self).__init__()
-        self.direction = direction
 
-
-class EncapsulationClassifier(Classifier):
-    __tablename__ = 'encapsulation_classifiers'
-    __mapper_args__ = {'polymorphic_identity': 'encapsulationclassifier'}
-    id = sa.Column(sa.String(36), sa.ForeignKey('classifiers.id'),
+class IPV6Classification(ClassificationBase):
+    __tablename__ = 'ipv6_classifications'
+    __mapper_args__ = {'polymorphic_identity': 'ipv6'}
+    id = sa.Column(sa.String(36), sa.ForeignKey('classifications.id'),
                    primary_key=True)
-    encapsulation_type = sa.Column(sa.Enum(*constants.ENCAPSULATION_TYPES))
-    encapsulation_id = sa.Column(sa.String(255))
+    dscp = sa.Column(sa.Integer())
+    dscp_mask = sa.Column(sa.Integer())
+    ecn = sa.Column(sa.Enum("0", "1", "2", "3"))
+    length_min = sa.Column(sa.Integer())
+    length_max = sa.Column(sa.Integer())
+    next_header = sa.Column(sa.Integer())
+    hops_min = sa.Column(sa.SmallInteger())
+    hops_max = sa.Column(sa.SmallInteger())
+    src_addr = sa.Column(sa.String(49))
+    dst_addr = sa.Column(sa.String(49))
 
 
-class EthernetClassifier(Classifier):
-    __tablename__ = 'ethernet_classifiers'
-    __mapper_args__ = {'polymorphic_identity': 'ethernetclassifier'}
-    id = sa.Column(sa.String(36), sa.ForeignKey('classifiers.id'),
+class EthernetClassification(ClassificationBase):
+    __tablename__ = 'ethernet_classifications'
+    __mapper_args__ = {'polymorphic_identity': 'ethernet'}
+    id = sa.Column(sa.String(36), sa.ForeignKey('classifications.id'),
                    primary_key=True)
-    ethertype = sa.Column(sa.Integer)
-    source_mac = sa.Column(sa.String(255))
-    destination_mac = sa.Column(sa.String(255))
+    ethertype = sa.Column(sa.Integer())
+    src_addr = sa.Column(sa.String(17))
+    dst_addr = sa.Column(sa.String(17))
 
 
-class IpClassifier(Classifier):
-    __tablename__ = 'ip_classifiers'
-    __mapper_args__ = {'polymorphic_identity': 'ipclassifier'}
-    id = sa.Column(sa.String(36), sa.ForeignKey('classifiers.id'),
+class UDPClassification(ClassificationBase):
+    __tablename__ = 'udp_classifications'
+    __mapper_args__ = {'polymorphic_identity': 'udp'}
+    id = sa.Column(sa.String(36), sa.ForeignKey('classifications.id'),
                    primary_key=True)
-    source_ip_prefix = sa.Column(sa.String(255))
-    destination_ip_prefix = sa.Column(sa.String(255))
+    src_port_min = sa.Column(sa.Integer)
+    src_port_max = sa.Column(sa.Integer)
+    dst_port_min = sa.Column(sa.Integer)
+    dst_port_max = sa.Column(sa.Integer)
+    length_min = sa.Column(sa.Integer())
+    length_max = sa.Column(sa.Integer())
 
 
-class Ipv4Classifier(Classifier):
-    __tablename__ = 'ipv4_classifiers'
-    __mapper_args__ = {'polymorphic_identity': 'ipv4classifier'}
-    id = sa.Column(sa.String(36), sa.ForeignKey('classifiers.id'),
+class TCPClassification(ClassificationBase):
+    __tablename__ = 'tcp_classifications'
+    __mapper_args__ = {'polymorphic_identity': 'tcp'}
+    id = sa.Column(sa.String(36), sa.ForeignKey('classifications.id'),
                    primary_key=True)
-    dscp_tag = sa.Column(sa.String(255))
-    protocol = sa.column(sa.Enum(*constants.PROTOCOLS))
-    dscp_mask = sa.Column(sa.String(255))
+    src_port_min = sa.Column(sa.Integer)
+    src_port_max = sa.Column(sa.Integer)
+    dst_port_min = sa.Column(sa.Integer)
+    dst_port_max = sa.Column(sa.Integer)
+    flags = sa.Column(sa.Integer())
+    flags_mask = sa.Column(sa.Integer())
+    window_min = sa.Column(sa.Integer())
+    window_max = sa.Column(sa.Integer())
 
 
-class Ipv6Classifier(Classifier):
-    __tablename__ = 'ipv6_classifiers'
-    __mapper_args__ = {'polymorphic_identity': 'ipv6classifier'}
-    id = sa.Column(sa.String(36), sa.ForeignKey('classifiers.id'),
-                   primary_key=True)
-    next_header = sa.Column(sa.Enum(*constants.PROTOCOLS))
-    traffic_class = sa.Column(sa.String(255))
-    flow_label = sa.Column(sa.String(255))
+def _read_classification_groups(svc_plu, context, id=None):
+    class_group = svc_plu._get_collection(context, ClassificationGroup,
+                                          _generate_dict_from_cg_db)
+    cg_m_c = svc_plu._get_collection(context, CGToClassificationMapping,
+                                     _generate_dict_from_cgmapping_db)
+    cg_m_cg = svc_plu._get_collection(context, CGToClassificationGroupMapping,
+                                      _generate_dict_from_cgmapping_db)
+    id_class = None
+    for cg in class_group:
+        class_ids = []
+        group_ids = []
+        if id and cg['id'] != id:
+            continue
+        for mapping in cg_m_c:
+            if cg['id'] == mapping['container_cg_id']:
+                class_ids.append(mapping['stored_classification_id'])
+        for mapping in cg_m_cg:
+            if cg['id'] == mapping['container_cg_id']:
+                group_ids.append(mapping['stored_cg_id'])
+        cg['classifications'] = ','.join(str(x) for x in class_ids)
+        cg['classification_groups'] = ','.join(str(x) for x in group_ids)
+        id_class = cg
+
+    if id:
+        return id_class
+    else:
+        return class_group
 
 
-class NeutronPortClassifier(Classifier):
-    __tablename__ = 'neutron_port_classifiers'
-    __mapper_args__ = {'polymorphic_identity': 'neutronportclassifier'}
-    id = sa.Column(sa.String(36), sa.ForeignKey('classifiers.id'),
-                   primary_key=True)
-    logical_source_port = sa.Column(sa.String(255))
-    logical_destination_port = sa.Column(sa.String(255))
+def _generate_dict_from_cg_db(model, fields=None):
+    resp = {}
+
+    resp['id'] = model.id
+    resp['name'] = model.name
+    resp['description'] = model.description
+    resp['project_id'] = model.project_id
+    resp['classifications'] = ''
+    resp['classification_groups'] = ''
+    resp['shared'] = model.shared
+    resp['operator'] = model.operator
+
+    return resp
 
 
-class TransportClassifier(Classifier):
-    __tablename__ = 'transport_classifiers'
-    __mapper_args__ = {'polymorphic_identity': 'transportclassifier'}
-    id = sa.Column(sa.String(36), sa.ForeignKey('classifiers.id'),
-                   primary_key=True)
-    source_port_range_max = sa.Column(sa.Integer)
-    source_port_range_min = sa.Column(sa.Integer)
-    destination_port_range_max = sa.Column(sa.Integer)
-    destination_port_range_min = sa.Column(sa.Integer)
+def _generate_dict_from_cgmapping_db(model, fields=None):
+    resp = {}
 
-    def __init__(self, source_port_range_min=None,
-                 source_port_range_max=None,
-                 destination_port_range_min=None,
-                 destination_port_range_max=None):
-        super(TransportClassifier, self).__init__()
-        self.destination_port_range_min = destination_port_range_min
-        self.destination_port_range_max = destination_port_range_max
-        self.source_port_range_min = source_port_range_min
-        self.source_port_range_max = source_port_range_max
+    resp['container_cg_id'] = model.container_cg_id
+    if 'stored_cg_id' in model:
+        resp['stored_cg_id'] = model.stored_cg_id
+    else:
+        resp['strored_classification_id'] = model.stored_classification_id
 
+    return resp
 
-class VlanClassifier(Classifier):
-    __tablename__ = 'vlan_classifiers'
-    __mapper_args__ = {'polymorphic_identity': 'vlanclassifier'}
-    id = sa.Column(sa.String(36), sa.ForeignKey('classifiers.id'),
-                   primary_key=True)
-    vlan_priority = sa.Column(sa.Integer)
+RESOURCE_MODELS = {'ipv4_classification': IPV4Classification,
+                   'ipv6_classification': IPV6Classification,
+                   'tcp_classification': TCPClassification,
+                   'udp_classification': UDPClassification,
+                   'ethernet_classification': EthernetClassification}
